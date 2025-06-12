9b5cdf4759d26f61a1318996764150a8cc8a47aa
tykyphatky <gg.fctaiphat@yahoo.com>
2025-06-12 16:56:12 +0700

    add get blog by id

--- BlogApi/BlogApi/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
+++ BlogApi/BlogApi/obj/Debug/net8.0/BlogApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfb0b31e24acf853e43f0b73b3ca8f892e6b40da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d5f7f218be1a5d21062ba8502288b84659456f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
