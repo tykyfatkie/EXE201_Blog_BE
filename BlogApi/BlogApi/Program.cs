13a8a1c58012ea8286a828b6dd1d410f646756b8
Phát Tài Tăng Nguyễn <92193419+tykyfatkie@users.noreply.github.com>
2025-06-12 19:44:40 +0700

    Update Program.cs

--- BlogApi/BlogApi/Program.cs
+++ BlogApi/BlogApi/Program.cs
@@ -66,14 +66,12 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog API v1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blog API v1");
+});
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowSpecificOrigins");  
